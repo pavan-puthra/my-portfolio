31739c3038cbb7b6ede9a62521d0f1812ec280a2
Pavan Puthra Reddy Thangella <pthangel@asu.edu>
2025-07-02 11:40:38 -0700

    dfdsf

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: "pavan-puthra.github.io/my-portfolio/",  // ✅ GitHub repo name
+  base: "/my-portfolio/",  // ✅ GitHub repo name
   server: {
     host: "0.0.0.0",
     port: 8080,
